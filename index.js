436e401cb0530cc498229087bed3a830ccb8f5a6
Diana Laura Ramos Aguikar <52227360+dianaRamosAg@users.noreply.github.com>
2019-07-02 10:41:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const usuarioController = require("./clientes.controller");
+const usuarioController = require("./productos.controller");
+const usuarioController = require("./venta.controller");
+const usuarioController = require("./marca.controller");
---
+++ venta.controller.js
@@ -0,0 +1,2 @@
+var mongoose = require("mongoose");
+var schema = require("./venta.model");
