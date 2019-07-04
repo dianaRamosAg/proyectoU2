1e071a34f30d16b7d09226c5aa3c163a1644bdd5
yanitzin <yanitzin97@gmail.com>
2019-07-03 18:13:01 -0600

    controller sale

--- sale.controller.js
+++ sale.controller.js
@@ -1,2 +1,57 @@
-var mongoose = require("mongoose");
-var schema = require("./venta.model");
+var productsController = require("./products.controller");
+var clientsController = require("./clients.controller");
+
+
+async function create(product, clients, Product, Clients) {
+
+    var product = {
+        product: product
+    };
+
+    var clients = {
+        clients: clients
+    };
+
+
+    var productCreated = await productsController.create(product, Product);
+    var clientsCreated = await clientsController.create(clients, Clients);
+
+    sale["clients"] = clientsCreated.name;
+    sale["products"] = productsCreated.cost;
+
+    var saleCreated = await Sale.create(sale)
+        .then((data) => {
+            console.log("Venta Guardada!!!");
+            // console.log(data);
+            return data;
+        })
+        .catch((error) => {
+            console.log("Error en guardar la venta!!!");
+            // console.log(error);
+            return error;
+        });
+    return saleCreated;
+}
+//Buscar 
+async function findByPrice(priceToFind, Sale) {
+
+    var params = {
+        price: priceToFind
+    }
+
+    var saleFind = await Sale.find(params)
+        .populate("products")
+        .then((data) => {
+            // console.log(data);
+            return data;
+        })
+        .catch((err) => {
+            console.log("Not found");
+            return err;
+        });
+
+    return saleFind;
+}
+
+module.exports.create = create;
+module.exports.findByPrice = findByPrice;
