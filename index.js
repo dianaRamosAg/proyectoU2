cd39856945dd9063cdb52d972d9a1a4f538b9b3b
dianaRamosAg <52227360+dianaRamosAg@users.noreply.github.com>
2019-07-03 19:02:06 -0500

    clients e index

--- index.js
+++ index.js
@@ -1,42 +1,67 @@
-const clienteController = require("./clientes.controller");
-const productosController = require("./productos.controller");
-const ventaController = require("./venta.controller");
-const marcaController = require("./marca.controller");
+const productsController = require("./products.controller");
+const clientsController = require("./clients.controller");
 
+var mongoose = require("mongoose");
+var brandSchema = require("./brand.model");
+var productSchema = require("./products.model");
 
-//Cliente
-var cliente = {
-    RFC:"GAFJ810702NA0",
-    nombre:"Diana Laura Ramos Aguilar",
-    domicilio:"calle 7 #19",
-    telefono:"3111255293",
-    email:"diana.laura9625@gmail.com"
-};
-//llamar crud del cliente
-clienteController.insertarC(cliente);
-clienteController.ConsultaCxN("Diana");
-clienteController.ConsultaCRFC('GAFJ810702NA0');
-clienteController.ActualizarCT('5552196165');
-clienteController.EliminarC('GAFJ810702NA0');
-clienteController.ActualizarCDom('Av Orquidea #55');
+mongoose.connect('mongodb://localhost:27017/bdAEU2', { useNewUrlParser: true }); //Conexión
 
+var Brand = mongoose.model('Brand', brandSchema, 'brands');
+var Product = mongoose.model('Product', productSchema, 'products');
 
-//Productos
-
-
-//Marcas
-var marca = {
-    nombre:"lala"
-};
-marcaController.insertarM(marca);
-
-//Ventas
-var venta={
-    cliente: "Diana Laura Ramos aguilar",
-    fecha: "02/07/2019",
-    subtotal: 145,
+async function createBrandAndProduct() {
+    var product = {
+        name: "MacBook Air",
+        price: 100,
+        cost: 50
+    };
+    
+    const brand = "Apple";
+    
+    var productCreated = await productsController.create(product, brand, Product, Brand);
+    console.log("------- Producto Creado --------");
+    console.log(productCreated);
 }
 
-clienteController.insertarv(cliente);
-clienteController.ConsultaF("02/07/2019");
-//clienteController.EliminarC('GAFJ810702NA0');
+async function findProduct(price) {
+    var productPrice = await productsController.findByPrice(price, Product)
+    console.log("------- Productos Encontrado --------");
+
+    console.log(productPrice);
+}
+ 
+createBrandAndProduct();
+findProduct(100);
+
+//------------ Cliente
+
+async function createClient() {
+    var client = {
+        RFC: "RINR9612211AM9",
+        name: 'Carlos Uriel Martinez',
+        address: 'Fracc. Jacarandas ',
+        cell:'311-219-61-65',
+        email:'diana.laura9625@gmail.com'
+
+    };
+    
+    var clientCreated = await clientsController.create(client);
+    console.log(" Cliente guardado ");
+    console.log(clientCreated);
+}
+
+async function findClient(RFC) {
+    var clientRFC = await clientsController.findByRFC(RFC)
+    console.log("------- Cliente Encontrado  por RFC--------");
+
+    console.log(clientRFC);
+}
+
+createClient();
+
+
+
+
+
+
